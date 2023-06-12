ca4d46ed93f1b41a48f4a973a6ec3a34e02fc9b5
ThapaVinay <thechampthapa@gmail.com>
2023-06-12 16:08:38 +0530

    api key is made hidden

--- src/components/News.js
+++ src/components/News.js
@@ -39,7 +39,7 @@ export class News extends Component {
 
     async updateNews() {
         this.props.setProgress(10);
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2f8228e3f6d64628afcb69b98a66bf61&page=${this.state.page} &pageSize=${this.props.pageSize}`
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page} &pageSize=${this.props.pageSize}`
         this.setState({ loading: true })
         let data = await fetch(url);
         this.props.setProgress(30);
@@ -79,7 +79,7 @@ export class News extends Component {
         this.setState({
             page: this.state.page + 1
         })
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2f8228e3f6d64628afcb69b98a66bf61&page=${this.state.page} &pageSize=${this.props.pageSize}`
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page} &pageSize=${this.props.pageSize}`
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
